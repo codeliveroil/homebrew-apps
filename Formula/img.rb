2526202720b8addf0a25f65a2ebff25742dcce52
codeliveroil <kushelrai@gmail.com>
2019-12-13 21:01:14 -0800

    Update img version to use Go modules.

--- Formula/img.rb
+++ Formula/img.rb
@@ -1,5 +1,5 @@
 class Img < Formula
-  @@ver = "v1.1"
+  @@ver = "1.2"
   @@repo = "img"
   @@tool = "img"
 
@@ -11,14 +11,7 @@ class Img < Formula
   depends_on "go" => :build
 
   def install
-    # build a go-like file hierarchy (e.g. src/github.com/organization/app)
-    # and throw our source in there. We need to do this because our source
-    # normally references vendors using the go file hierarchy
-    ENV["GOPATH"] = buildpath
-    path = buildpath/"src/github.com/codeliveroil/#{@@repo}"
-    path.install Dir["*"]
-
-    cd "#{path}" do
+    cd "#{buildpath}" do
       system "go", "build", "-o", bin/"#{@@tool}"
     end
   end
