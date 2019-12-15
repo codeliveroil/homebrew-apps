6617a6680953d96e7541c6f95c9f7c5ae060300a
codeliveroil <kushelrai@gmail.com>
2019-12-14 23:46:51 -0800

    Add formula for clr.

---
+++ Formula/clr.rb
@@ -0,0 +1,19 @@
+class Clr < Formula
+  @@ver = "1.0"
+  @@repo = "clr"
+  @@tool = "clr"
+
+  desc "Output colorizer"
+  homepage "https://github.com/codeliveroil/#{@@repo}"
+  url "https://github.com/codeliveroil/#{@@repo}.git", :tag => @@ver, :using => :git
+  version @@ver
+
+  depends_on "go" => :build
+
+  def install
+    cd "#{buildpath}" do
+      system "go", "build", "-o", bin/"#{@@tool}"
+    end
+  end
+end
+
